635cf87212e9fe5da6195759aaf7196fbc97c452
ciroanni <annicchiaricociro09@icloud.com>
2023-08-22 11:34:43 +0200

    pds_project v1

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,4 +1,4 @@
-use druid::widget::{Button, CrossAxisAlignment, Flex, Label};
+use druid::widget::{Button, CrossAxisAlignment, Flex, Label, TextBox};
 use druid::PlatformError;
 use druid::{AppLauncher, Data, Lens, LocalizedString, Widget, WidgetExt, WindowDesc};
 
@@ -13,10 +13,12 @@ fn build_ui() -> impl Widget<AppState> {
             TextBox::new()
                 .with_placeholder("screen.jpeg")
                 .expand_width()
-                .lens(AppState::new_todo),
+                .lens(AppState::name)
         )
         .with_spacer(20.0)
-        .with_child(Button::new("+ Nuovo").on_click(|ctx, data: &mut AppState, _env| {}))
+        .with_child(Button::new("+ Nuovo").on_click(|ctx, data: &mut AppState, _env| {
+            data.screen();
+        }))
 }
 
 fn main() -> Result<(), PlatformError> {
@@ -25,4 +27,64 @@ fn main() -> Result<(), PlatformError> {
         .window_size((300.0, 200.0));
 
     AppLauncher::with_window(main_window).launch(AppState { name: "".to_string()})
+}
+
+impl AppState {
+    fn screen(&mut self) {
+        let a = screenshots::DisplayInfo::all();
+
+        let display_info = match a {
+            Err(why) => return println!("{}", why),
+            Ok(info) => info,
+        };
+
+        println!("{:?}", display_info);
+
+        let b = screenshots::Screen::new(&display_info[0]);
+
+        println!("{:?}", b);
+
+        let c = b.capture();
+        //let d=b.capture_area(0, 0, 100, 100);
+
+        let image = match c {
+            Err(why) => return println!("{}", why),
+            Ok(info) => info,
+        };
+
+        let ok = image.to_png(None);
+
+        let immagine = match ok {
+            Err(why) => return println!("{}", why),
+            Ok(data) => data,
+        };
+
+        println!("lunghezza vettore immagine:{}", immagine.len());
+
+        let f = image::guess_format(&immagine);
+        let format = match f {
+            Err(why) => return println!("{}", why),
+            Ok(data) => data,
+        };
+
+        println!("{:?}", format);
+
+        let scale_factor = display_info[0].scale_factor;
+        let width = display_info[0].width as f32;
+        let height = display_info[0].height as f32;
+
+
+        let e = image::save_buffer_with_format(
+            self.name.as_str(),
+            image.rgba(),
+            (width * scale_factor) as u32,
+            (height * scale_factor) as u32,
+            image::ColorType::Rgba8,
+            image::ImageFormat::Png,
+        );
+        match e {
+            Err(why) => return println!("errore:{}", why),
+            Ok(()) => return,
+        };
+    }
 }
---
+++ project/target/debug/deps/screenshots-598067aa2548fc32.d
@@ -0,0 +1,9 @@
+C:\Users\Ciro\Desktop\rust_folder\pds_project\screen-grabbing-utility\project\target\debug\deps\screenshots-598067aa2548fc32.rmeta: C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\lib.rs C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\image.rs C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\win32.rs
+
+C:\Users\Ciro\Desktop\rust_folder\pds_project\screen-grabbing-utility\project\target\debug\deps\libscreenshots-598067aa2548fc32.rlib: C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\lib.rs C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\image.rs C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\win32.rs
+
+C:\Users\Ciro\Desktop\rust_folder\pds_project\screen-grabbing-utility\project\target\debug\deps\screenshots-598067aa2548fc32.d: C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\lib.rs C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\image.rs C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\win32.rs
+
+C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\lib.rs:
+C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\image.rs:
+C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\screenshots-0.7.3\src\win32.rs:
