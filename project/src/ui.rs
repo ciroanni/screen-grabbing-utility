48308e47bcb339c192df3b1048cab9cb586c2b51
ciroanni <annicchiaricociro09@icloud.com>
2023-08-22 21:12:42 +0200

    v1.0.1 with radiogroups

--- project/src/ui.rs
+++ project/src/ui.rs
@@ -1,7 +1,9 @@
 use crate::data::*;
-use druid::widget::{Button, Flex, Label, Radio, RadioGroup, TextBox, Align};
+use druid::widget::{Align, Button, Flex, Label, Radio, RadioGroup, TextBox, CrossAxisAlignment};
 use druid::PlatformError;
-use druid::{im::Vector, AppLauncher, Data, Lens, LocalizedString, Widget, WidgetExt, WindowDesc, UnitPoint};
+use druid::{
+    im::Vector, AppLauncher, Data, Lens, LocalizedString, UnitPoint, Widget, WidgetExt, WindowDesc,
+};
 use image::ImageFormat;
 
 pub fn build_ui() -> impl Widget<AppState> {
@@ -22,26 +24,27 @@ pub fn build_ui() -> impl Widget<AppState> {
 }
 
 pub fn radiogroup() -> impl Widget<AppState> {
-    Flex::column()
-        .with_child(Align::centered(Radio::new(
+    Flex::row()
+        .cross_axis_alignment(CrossAxisAlignment::Center)
+        .with_child(Radio::new(
             "Jpeg",
             AppState {
                 name: "".to_string(),
                 format: ".jpeg".to_string(),
             },
-        )))
-        .with_child(Align::centered(Radio::new(
+        ))
+        .with_child(Radio::new(
             "Png",
             AppState {
                 name: "".to_string(),
                 format: ".png".to_string(),
             },
-        )))
-        .with_child(Align::centered(Radio::new(
+        ))
+        .with_child(Radio::new(
             "Gif",
             AppState {
                 name: "".to_string(),
                 format: ".gif".to_string(),
             },
-        )))
+        ))
 }
