5fe21218885d9df2604eefc3100de61ff20bfb2f
ciroanni <annicchiaricociro09@icloud.com>
2023-08-24 16:55:59 +0200

    v1.03 with more formats

---
+++ project/target/debug/build/druid-shell-bf931d1e7623fd9b/build_script_build-bf931d1e7623fd9b.d
@@ -0,0 +1,5 @@
+C:\Users\Ciro\Desktop\rust_folder\pds_project\screen-grabbing-utility\project\target\debug\build\druid-shell-bf931d1e7623fd9b\build_script_build-bf931d1e7623fd9b.exe: C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\druid-shell-0.8.3\build.rs
+
+C:\Users\Ciro\Desktop\rust_folder\pds_project\screen-grabbing-utility\project\target\debug\build\druid-shell-bf931d1e7623fd9b\build_script_build-bf931d1e7623fd9b.d: C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\druid-shell-0.8.3\build.rs
+
+C:\Users\Ciro\.cargo\registry\src\github.com-1ecc6299db9ec823\druid-shell-0.8.3\build.rs:
